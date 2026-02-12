c540c78363b37fc1c69b066fc2ee0f22936b10a7
laidlaw_dev <laidlaw_dev@btinternet.com>
2026-02-12 12:42:12 +0000

    initial project

---
+++ README.md
@@ -0,0 +1,9 @@
+# vimes
+Simple functional programming language
+
+## Author
+laidlaw_dev
+
+## Purpose
+This repo is the handcoded version of the Vimes functional programming language. All code is hand written by a human (me). It's a comparison project
+to the vimes repo that was entirely coded by AI.
---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      eslintConfigPrettier,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      'no-unused-vars': ['off'],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+    },
+  },
+  {
+    files: ['**/*.test.{ts,tsx}'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+]);
